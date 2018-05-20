8dc5f3b8189fce58eb1f48eba0585cd957674787
zentech <jlvazquez825@gmail.com>
2018-05-20 18:58:34 -0400

    finish adding buttons

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-$(document).ready(function() {  
-  $("#twitter-btn").hide();
+$(document).ready(function() { 
+  var socialBtns = document.getElementById("buttons");
+  socialBtns.style.display = "none";
   /*getting random quote on button click*/
   $('#getMessage').on("click", function() {
     $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?", function(json) {      
@@ -7,9 +8,8 @@ $(document).ready(function() {
       console.log(json.quoteAuthor);
       // var quoteArr = json.contents.quotes[0])
      $("#content").html('\"'+json.quoteText+'\"');
-    $("#author").html("--"+json.quoteAuthor);
-      $("#twitter-btn").show();
-      
+    $("#author").html("--"+json.quoteAuthor);     
+      socialBtns.style.display = "inline";
       /*preparing href link for twitter button*/
       var quot = 'https://twitter.com/intent/tweet?text='+ json.quoteText + " "+json.quoteAuthor;
       $(".btn-twitter").attr("href", quot);
